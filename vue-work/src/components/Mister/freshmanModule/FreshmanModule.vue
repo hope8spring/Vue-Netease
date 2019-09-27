36573a4875e10c37858725c7ba69cf179fabdca0
hope8spring@163.com <hope8spring@163.com>
2019-09-27 16:48:22 +0800

    增加海报模块

--- vue-work/src/components/Mister/freshmanModule/FreshmanModule.vue
+++ vue-work/src/components/Mister/freshmanModule/FreshmanModule.vue
@@ -16,8 +16,8 @@
       </div>
     </div>
     <div class="rightBottom">
-      <span class="textA">福利社</span>
-      <span class="textB">今日特价</span>
+      <div  class="textA">福利社</div>
+      <div class="textB">今日特价</div>
       <img class="img" src="https://yanxuan-item.nosdn.127.net/5e948fbad2f43381444a150848c4dd1a.png?imageView&thumbnail=200x200&quality=75" alt="">
       <div class="round">
         <span class="textA">￥20.9</span>
@@ -36,17 +36,76 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../../../common/stylus.styl"
   .Freshman
+    width rem(750)
+    display inline-block
     .FreshmanTitle
+      font-size rem(32)
+      height rem(80)
+      width rem(750)
+      text-align center
+      line-height rem(80)
+      color darkslategrey
+      margin-top rem(-20)
     .left
+      float left
+      width rem(350)
+      height rem(300)
+      background-color #F9E9CF
+      display inline-block
+      position relative
       .text
+        color #333
+        font-size rem(32)
+        margin-left rem(20)
+        margin-top rem(20)
+        display block
       .img
+        width rem(200)
+        height rem(200)
+        position absolute
+        top 0
+        bottom rem(-20)
+        right  0
+        left 0
+        margin auto
     .rightTop
+      width rem(350)
+      height rem(145)
+      float right
+      background-color #2fb5dd
       .textA
+        font-size rem(24)
       .textB
+        font-size rem(12)
       .img
+        width rem(200)
+        height rem(132)
+        text-align center
+        line-height rem(132)
       .round
+        zoom 1
         .textA
+          font-size rem(24)
         .textB
+          font-size rem(24)
+    .rightBottom
+      float right
+      width rem(350)
+      height rem(145)
+      background-color #0d3add
+      .textA
+        font-size rem(24)
+      .textB
+        font-size rem(24)
+      .img
+        width rem(187)
+        height rem(132)
+      .round
 
+        .textA
+          font-size rem(10)
+        .textB
+          font-size rem(10)
 </style>
