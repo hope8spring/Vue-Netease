839e02e56cf2ac742094bee58b4893cce4e1c180
hope8spring@163.com <hope8spring@163.com>
2019-09-27 16:50:31 +0800

    增加海报模块

---
+++ Vue-pre/小工具/0425ip设置.bat
@@ -0,0 +1,14 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+ 
+netsh interface ipv4 set address name="ÒÔÌ«Íø" source=static addr=192.168.3.221 mask=255.255.255.0 gateway=192.168.3.1
